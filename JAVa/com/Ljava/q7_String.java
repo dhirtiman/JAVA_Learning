cf7b493b27d4e03ea1f701805eb515ebf015c325
Dhirtiman Khati <108656037+dhirtiman@users.noreply.github.com>
2023-03-04 02:47:20 +0530

    Update q7_String.java

--- JAVa/com/Ljava/q7_String.java
+++ JAVa/com/Ljava/q7_String.java
@@ -1,4 +1,5 @@
 package com.Ljava;
+
 import java.lang.String;
 
 public class q7_String {
@@ -6,67 +7,39 @@ public class q7_String {
     public static void main(String args[]) {
 
         StringBuffer s = new StringBuffer("books");
-        System.out.println("The String: "+s);
+        System.out.println("The String: " + s + "\n");
 
-        System.out.println("");
-
-        s.setCharAt(0,'h');
+        s.setCharAt(0, 'h');
         System.out.println("setting 'h' at index 0");
-        System.out.println("The String: "+s);
-
-        System.out.println("");
+        System.out.println("The String: " + s + "\n");
 
         s.setLength(4);
         System.out.println("Setting string length to 4");
-        System.out.println("The String: "+s);
-
-        System.out.println("");
+        System.out.println("The String: " + s + "\n");
 
         s.append('s');
         System.out.println("Appending 's' in the string");
-        System.out.println("The String: "+s);
+        System.out.println("The String: " + s + "\n");
 
-        System.out.println("");
-
-        s.insert(4,'e');
+        s.insert(4, 'e');
         System.out.println("adding 'e' at the offset of index 4");
-        System.out.println("The String: "+s);
-
-        System.out.println("");
-        
+        System.out.println("The String: " + s + "\n");
 
         String s1 = new String("Pointy ");
-        System.out.println("String 1: "+s1);
+        System.out.println("String 1: " + s1);
         String s2 = new String("Swords ");
-        System.out.println("String 2: "+s2);
+        System.out.println("String 2: " + s2);
 
-        System.out.println("");
-
-        System.out.println("Concatinating "+s1+" and "+s2);
+        System.out.println("Concatinating " + s1 + " and " + s2);
         System.out.println(s1.concat(s2));
 
-        System.out.println("");
-
         System.out.println("Comparing String 1 and String 2");
-        if(s1.equals(s2))
-        {
-            System.out.println(s1+" and "+s2+"are equal");
+        if (s1.equals(s2)) {
+            System.out.println(s1 + " and " + s2 + "are equal");
 
+        } else {
+            System.out.println(s1 + "and " + s2 + "are not equal");
         }
-        else
-        {
-            System.out.println(s1+"and "+s2+"are not equal");
-        }
-
-
-
-
-        
-
-
-
-
-
 
     }
 
